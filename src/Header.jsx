6ef465b0496c40584235d6fecc88240c98f9fd34
jpfairhart <johnpaulfairhart@hotmail.com>
2024-01-01 23:13:25 -0800

    add initial header text for name of site and contact info

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+
+  return (
+    <>
+      <header>
+        <h1>Delaney's Designs</h1>
+        <span>Contact: (555) 555-5555</span>
+      </header>
+    </>
+  )
+}
+
+export default Header
