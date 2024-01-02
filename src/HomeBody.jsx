9e0ec3943ccca5eb565fd63a98666d19709d2a25
jpfairhart <johnpaulfairhart@hotmail.com>
2024-01-01 23:30:49 -0800

    add basic hero text structure

---
+++ src/HomeBody.jsx
@@ -0,0 +1,15 @@
+import './HomeBody.css'
+
+function HomeBody() {
+
+  return (
+    <>
+      <section id='hero'>
+        <h2>Custom beaded bracelets and keychains</h2>
+        <span>Text for more information</span>
+      </section>
+    </>
+  )
+}
+
+export default HomeBody
