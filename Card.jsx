fb8bc46edfb5c3bb35a68bd6abf2fb5a9e781faa
jpfairhart <johnpaulfairhart@hotmail.com>
2024-01-14 22:23:39 -0800

    add basic card element structure

---
+++ Card.jsx
@@ -0,0 +1,17 @@
+import './Card.css'
+
+function Card() {
+
+  return (
+    <>
+      <article>
+        <img></img>
+        <h3>Product Title</h3>
+        <h3>Product Price</h3>
+        <p>This product is amazing!</p>
+      </article>
+    </>
+  )
+}
+
+export default Card
